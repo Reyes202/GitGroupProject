f7b4b1225d501e7bcf6db9d443a31eb53350d02a
Hector <reyesbamaca9@gmail.com>
2022-08-30 21:48:28 -0400

    Water Class Updated

--- src/Package1/Water.java
+++ src/Package1/Water.java
@@ -3,5 +3,8 @@ package Package1;
 public class Water {
   public static void main(String[] args) {
     System.out.println("water class created");
+
+
+    System.out.println("water class updated");
   }
 }
