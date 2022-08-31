e5cc1fbaac987d9801b4d2807fe955326ad01ef4
Hector <reyesbamaca9@gmail.com>
2022-08-30 21:51:43 -0400

    Food Class Updated

--- src/Package1/Food.java
+++ src/Package1/Food.java
@@ -3,5 +3,6 @@ package Package1;
 public class Food {
     public static void main(String[] args) {
         System.out.println("Food class is created");
+        System.out.println("updated");
     }
 }
