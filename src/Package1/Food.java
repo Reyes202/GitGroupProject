f7d2d35e877a87ac8e25e4798e706d784b3e9573
Hector <reyesbamaca9@gmail.com>
2022-08-30 21:52:14 -0400

    Food Class Completed

--- src/Package1/Food.java
+++ src/Package1/Food.java
@@ -4,5 +4,6 @@ public class Food {
     public static void main(String[] args) {
         System.out.println("Food class is created");
         System.out.println("updated");
+        System.out.println("completed");
     }
 }
